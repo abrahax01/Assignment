bdbda85e0ffcb7c00948e85edae7b1bb1cdf63bf
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-03-21 11:38:55 +0000

    Update Control.java

--- Control.java
+++ Control.java
@@ -3,6 +3,18 @@ public class Control
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        ReadData csv = new ReadData("MLdata.csv");
+        csv.openFile();
+        csv.readFile();
+        
+        String[][] stored = csv.getDataByRow();
+        int urban = csv.getUrban();
+
+        for(int i = 0; i < 6; i++)
+        {
+            System.out.print(stored[3][i] + " ");
+            
+        }
+        System.out.println("\n" + urban);
     }
 }
