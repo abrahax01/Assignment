2059dbad48cf6201feab230defc7c8d7211c1d63
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-03-31 00:09:38 +0100

    Update MachineLearning.java

--- MachineLearning.java
+++ MachineLearning.java
@@ -1,4 +1,22 @@
+// NAIVE BAYES ALGORITHM
 public class MachineLearning extends ReadData
 {
+    private String gender, ownBusiness, partTimeJob, area, studyBusiness, entrepreneur;
+
+    public MachineLearning(String[][] stored, String fileName) 
+    {   
+        super(fileName);  
+        ReadData data = new ReadData(fileName);
+        data.openFile();
+        data.readFile();
+        int totalData = data.getTotalData();
+        int genderMale = data.getGenderMale();
+        int genderFemale = data.getGenderFemale();
+         
+        // PROBABILITY 
+        System.out.println("Test " +  stored[3][0] + " " + totalData + " " + genderMale + " " + genderFemale);
+
+
+    }
     
 }
