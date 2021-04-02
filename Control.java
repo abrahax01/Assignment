de329bbcaf8d3194a0933e865149f6a624ed0dce
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-04-02 14:27:29 +0100

    Update Control.java

--- Control.java
+++ Control.java
@@ -1,3 +1,5 @@
+import java.util.Hashtable;
+
 // Will try to do Machine learning
 public class Control 
 {
@@ -6,8 +8,29 @@ public class Control
         ReadData csv = new ReadData("MLdata.csv");
         csv.openFile();
         csv.readFile();
+        String[][] dataByRow = csv.getDataByRow();
+       
+        Hashtable<String, Integer> dataDictionary = new Hashtable<String, Integer>();
+
+        dataDictionary.put("genderMale", csv.getGenderMale());
+        dataDictionary.put("genderFemale", csv.getGenderFemale());
+        dataDictionary.put("yesOwnBusiness", csv.getYesOwnBusiness());
+        dataDictionary.put("noOwnBusiness", csv.getNoOwnBusiness());
+        dataDictionary.put("yesJob", csv.getYesJob());
+        dataDictionary.put("noJob", csv.getNoJob());
+        dataDictionary.put("rural", csv.getRural());
+        dataDictionary.put("urban", csv.getUrban());
+        dataDictionary.put("yesStudyBusiness", csv.getYesStudyBusiness());
+        dataDictionary.put("noStudyBusiness", csv.getNoStudyBusiness());
+        dataDictionary.put("yesEntrepreneur", csv.getYesEntrepreneur());
+        dataDictionary.put("noEntrepreneur", csv.getNoEntrepreneur());
+        dataDictionary.put("totalData", csv.getTotalData());
+
+        System.out.println(csv.getTotalData());
+
+        MachineLearning naiveBayes = new MachineLearning("Male", "Yes", "Yes", "Urban", "Yes", dataDictionary, dataByRow);
     
-        csv.naiveBayes("Female", "Yes", "No", "Rural", "Yes");
+        // csv.naiveBayes("Male", "Yes", "Yes", "Urban", "Yes");
 
         // String[][] stored = csv.getDataByRow();
         // for(int i = 0; i < 6; i++)
