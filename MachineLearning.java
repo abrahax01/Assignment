7dd98cdc69ae1f818d73a7e19877e423677fa66d
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-04-03 18:35:45 +0100

    Update MachineLearning.java

--- MachineLearning.java
+++ MachineLearning.java
@@ -3,47 +3,35 @@ import java.util.Hashtable;
 // NAIVE BAYES ALGORITHM
 public class MachineLearning
 {
-    Hashtable<String, Integer> probabilityYesEntrepreneur = new Hashtable<String, Integer>(20);
-    Hashtable<String, Integer> probabilityNoEntrepreneur = new Hashtable<String, Integer>(20); 
+    Hashtable<String, Double> probabilitiesYesEntrepreneur = new Hashtable<String, Double>(20);
+    Hashtable<String, Double> probabilitiesNoEntrepreneur = new Hashtable<String, Double>(20); 
+    double probabilityYesEntrepreneur = 0, probabilityNoEntrepreneur = 0;
+    double numerator1 = 0, numerator0 = 0, denominator = 0;
+    String answer;
+    double[] toReturn = new double[2];
 
     // NAIVE BAYES 
     public MachineLearning(String gender, String ownBusiness, String partTimeJob, String area, String studyBusiness, Hashtable<String, Integer> dataDictonary, String[][] dataByRow) 
     {
-        // PRINT TESTS
-        System.out.println("Value at key = totalData: " + dataDictonary.get("totalData"));
-
-        // for(int i = 0; i < 10; i++)
-        // {
-        //     for(int j = 0; j < 6; j++)
-        //     {
-        //         System.out.print(dataByRow[i][j] + " ");
-        //     }
-        //     System.out.println("");
-        // }
-        // System.out.println("Male");
-
         // PROBABILITY OF PRIOR PROBABILITIES (ENTREPRENEUR)
-        double P_Y_Entrepreneur = (double)dataDictonary.get("yesEntrepreneur") / (double)dataDictonary.get("totalData"); // 0.56
-        double P_N_Entrepreneur = (double)dataDictonary.get("noEntrepreneur") / (double)dataDictonary.get("totalData");// 0.44
+        double P_Y_Entrepreneur = (double)dataDictonary.get("yesEntrepreneur") / (double)dataDictonary.get("totalData"); //.56
+        double P_N_Entrepreneur = (double)dataDictonary.get("noEntrepreneur") / (double)dataDictonary.get("totalData"); //.44
         
-
         // // PROBABILITY OF EVIDENCE DENOMINATOR 
-        // double PX_Male = (double)dataDictonary.get("genderMale") / (double)dataDictonary.get("totalData"); // 0.48
-        // double PX_Female = (double)dataDictonary.get("genderFemale") / (double)dataDictonary.get("totalData"); // 0.52
+        double PX_Male = (double)dataDictonary.get("genderMale") / (double)dataDictonary.get("totalData"); //.48
+        double PX_Female = (double)dataDictonary.get("genderFemale") / (double)dataDictonary.get("totalData"); //.52
 
-        // double PX_Y_OwnBusiness = (double)dataDictonary.get("yesOwnBusiness") / (double)dataDictonary.get("totalData"); // 0.44
-        // double PX_N_OwnBusiness = (double)dataDictonary.get("noOwnBusiness") / (double)dataDictonary.get("totalData"); // 0.56
+        double PX_Y_OwnBusiness = (double)dataDictonary.get("yesOwnBusiness") / (double)dataDictonary.get("totalData"); //.44
+        double PX_N_OwnBusiness = (double)dataDictonary.get("noOwnBusiness") / (double)dataDictonary.get("totalData"); //.56
 
-        // double PX_Y_PartTimeJob = (double)dataDictonary.get("yesJob") / (double)dataDictonary.get("totalData"); // 0.49 
-        // double PX_N_PartTimeJob = (double)dataDictonary.get("noJob") / (double)dataDictonary.get("totalData"); // 0.51
+        double PX_Y_PartTimeJob = (double)dataDictonary.get("yesJob") / (double)dataDictonary.get("totalData"); //.49
+        double PX_N_PartTimeJob = (double)dataDictonary.get("noJob") / (double)dataDictonary.get("totalData");//.51
 
-        // double PX_Rural = (double)dataDictonary.get("rural") / (double)dataDictonary.get("totalData"); // 0.50 
-        // double PX_Urban = (double)dataDictonary.get("urban") / (double)dataDictonary.get("totalData"); // 0.50
+        double PX_Rural = (double)dataDictonary.get("rural") / (double)dataDictonary.get("totalData"); //.50
+        double PX_Urban = (double)dataDictonary.get("urban") / (double)dataDictonary.get("totalData"); //.50
 
-        // double PX_Y_StudyBusiness = (double)dataDictonary.get("yesStudyBusiness") / (double)dataDictonary.get("totalData"); // 0.49 
-        // double PX_N_StudyBusiness = (double)dataDictonary.get("noStudyBusiness") / (double)dataDictonary.get("totalData"); // 0.51 
-
-        // int currentDataWithSameFeaturesAndYesEntrepreneur = 0, currentDataWithSameFeaturesAndNoEntrepreneur = 0;
+        double PX_Y_StudyBusiness = (double)dataDictonary.get("yesStudyBusiness") / (double)dataDictonary.get("totalData");//.49
+        double PX_N_StudyBusiness = (double)dataDictonary.get("noStudyBusiness") / (double)dataDictonary.get("totalData");//.51
 
         // TOTALS FOR FEATURES WHEN THEY BECAME ENTREPRENEURS (1 IN THE NAME MEANS THEY ARE ENTREPRENEURS)
         int male1 = 0, female1 = 0;
@@ -151,125 +139,134 @@ public class MachineLearning
                     noStudyBusiness0++;
                 } 
 
+
             } // END INNER ELSE IF STATEMENT
 
         } // END INNER FOR LOOP
 
-        // COMPARE DATA SENT WITH CURRENT DATA
-        // if(gender.equals(element[0]) && ownBusiness.equals(element[1]) && partTimeJob.equals(element[2]) && area.equals(element[3]) && studyBusiness.equals(element[4]) && "Yes".equals(element[5]))
-        // {
-        //     currentDataWithSameFeaturesAndYesEntrepreneur++;
-        // }
-        // else if(gender.equals(element[0]) && ownBusiness.equals(element[1]) && partTimeJob.equals(element[2]) && area.equals(element[3]) && studyBusiness.equals(element[4]) && "No".equals(element[5]))
-        // {
-        //     currentDataWithSameFeaturesAndNoEntrepreneur++;
-        // }
+        // PROBABILITY OF ENTREPENEUR 
+        probabilitiesYesEntrepreneur.put("maleProbability1", (male1 / (double)dataDictonary.get("yesEntrepreneur"))); //.23
+        probabilitiesYesEntrepreneur.put("femaleProbability1", (female1 / (double)dataDictonary.get("yesEntrepreneur"))); // .77
+        probabilitiesYesEntrepreneur.put("yesOwnBusinessProbability1", (yesOwnBusiness1 / (double)dataDictonary.get("yesEntrepreneur"))); //.63 
+        probabilitiesYesEntrepreneur.put("noOwnBusinessProbability1", (noOwnBusiness1 / (double)dataDictonary.get("yesEntrepreneur"))); //.37
+        probabilitiesYesEntrepreneur.put("yesPartTimeJob1", (yesPartTimeJob1 / (double)dataDictonary.get("yesEntrepreneur"))); //.46 
+        probabilitiesYesEntrepreneur.put("noPartTimeJob1", (noPartTimeJob1 / (double)dataDictonary.get("yesEntrepreneur")));  //.54
+        probabilitiesYesEntrepreneur.put("urban1", (urban1 / (double)dataDictonary.get("yesEntrepreneur")));  //.54
+        probabilitiesYesEntrepreneur.put("rural1", (rural1 / (double)dataDictonary.get("yesEntrepreneur")));   //.46
+        probabilitiesYesEntrepreneur.put("yesStudyBusiness1", (yesStudyBusiness1 / (double)dataDictonary.get("yesEntrepreneur")));  //.52 
+        probabilitiesYesEntrepreneur.put("noStudyBusiness1", (noStudyBusiness1 / (double)dataDictonary.get("yesEntrepreneur")));  //.48
 
-        // PROBABILITY OF LIKELIHOOD FOR YES ENTREPRENEUR
-        //162 YESES OF 290 -3
-        
-        // double P_X1_Male = (double) male_Y_Entrepreneur1 / (double) yesEntrepreneur; // 0.23
-        // double P_X1_Female = (double) female_Y_Entrepreneur1 / (double) yesEntrepreneur; // 0.77 
+        // PROBABILITY OF NO ENTREPENEUR  
+        probabilitiesNoEntrepreneur.put("maleProbability0", (male0 / (double)dataDictonary.get("noEntrepreneur"))); //.80
+        probabilitiesNoEntrepreneur.put("femaleProbability0", (female0 / (double)dataDictonary.get("noEntrepreneur"))); //.20
+        probabilitiesNoEntrepreneur.put("yesOwnBusinessProbability0", (yesOwnBusiness0 / (double)dataDictonary.get("noEntrepreneur"))); //.19
+        probabilitiesNoEntrepreneur.put("noOwnBusinessProbability0", (noOwnBusiness0 / (double)dataDictonary.get("noEntrepreneur"))); //.81
+        probabilitiesNoEntrepreneur.put("yesPartTimeJob0", (yesPartTimeJob0 / (double)dataDictonary.get("noEntrepreneur"))); //.54 
+        probabilitiesNoEntrepreneur.put("noPartTimeJob0", (noPartTimeJob0 / (double)dataDictonary.get("noEntrepreneur")));  //.46
+        probabilitiesNoEntrepreneur.put("urban0", (urban0 / (double)dataDictonary.get("noEntrepreneur")));  //.44
+        probabilitiesNoEntrepreneur.put("rural0", (rural0 / (double)dataDictonary.get("noEntrepreneur")));   //.56
+        probabilitiesNoEntrepreneur.put("yesStudyBusiness0", (yesStudyBusiness0 / (double)dataDictonary.get("noEntrepreneur")));   //.44
+        probabilitiesNoEntrepreneur.put("noStudyBusiness0", (noStudyBusiness0 / (double)dataDictonary.get("noEntrepreneur")));  //.56
 
-        // double P_X2_MaleYesOwnBusiness = (double) male_Y_OwnBusiness1 / (double) yesEntrepreneur; // 0.06
-        // double P_X2_MaleNoOwnBusiness = (double) male_N_OwnBusiness1 / (double) yesEntrepreneur; // 0.17
-        // double P_X2_FemaleYesOwnBusiness = (double) female_Y_OwnBusiness1 / (double) yesEntrepreneur; // 0.57
-        // double P_X2_FemaleNoOwnBusiness = (double) female_N_OwnBusiness1 / (double) yesEntrepreneur; // 0.20
-
-        // double P_X3_MaleYesJob = (double) male_Y_PartTimeJob1 / (double) yesEntrepreneur; // 0.07
-        // double P_X3_MaleNoJob = (double) male_N_PartTimeJob1 / (double) yesEntrepreneur; // 0.17 
-        // double P_X3_FemaleYesJob = (double) female_Y_PartTimeJob1 / (double) yesEntrepreneur; // 0.39
-        // double P_X3_FemaleNoJob = (double) female_N_PartTimeJob1 / (double) yesEntrepreneur; // 0.38 
-
-        // double P_X4_MaleUrban = (double) maleUrban1 / (double) yesEntrepreneur; // 0.17
-        // double P_X4_MaleRural = (double) maleRural1 / (double) yesEntrepreneur; // 0.06
-        // double P_X4_FemaleUrban = (double) femaleUrban1 / (double) yesEntrepreneur; // 0.37
-        // double P_X4_FemaleRural = (double) femaleRural1 / (double) yesEntrepreneur; // 0.40
-
-        // double P_X5_MaleYesStudyBusiness = (double) male_Y_StudyBusiness1 / (double) yesEntrepreneur; // 0.08
-        // double P_X5_MaleNoStudyBusiness = (double) male_N_StudyBusiness1 / (double) yesEntrepreneur; // 0.15
-        // double P_X5_FemaleYesStudyBusiness = (double) female_Y_StudyBusiness1 / (double) yesEntrepreneur; // 0.44
-        // double P_X5_FemaleNoStudyBusiness = (double) female_N_StudyBusiness1 / (double) yesEntrepreneur; // 0.32
-
-
-        // // PROBABILITY OF LIKELIHOOD FOR NO ENTREPRENEUR
-        // double P_X6_Male = (double) male_N_Entrepreneur0 / (double) noEntrepreneur; // 0.80
-        // double P_X6_Female = (double) female_N_Entrepreneur0 / (double) noEntrepreneur; // 0.20 
-
-        // double P_X7_MaleYesOwnBusiness = (double) male_Y_OwnBusiness0 / (double) noEntrepreneur; // 0.15
-        // double P_X7_MaleNoOwnBusiness = (double) male_N_OwnBusiness0 / (double) noEntrepreneur; // 0.65
-        // double P_X7_FemaleYesOwnBusiness = (double) female_Y_OwnBusiness0 / (double) noEntrepreneur; // 0.04
-        // double P_X7_FemaleNoOwnBusiness = (double) female_N_OwnBusiness0 / (double) noEntrepreneur; // 0.16
-
-        // double P_X8_MaleYesJob = (double) male_Y_PartTimeJob0 / (double) noEntrepreneur; // 0.52
-        // double P_X8_MaleNoJob = (double) male_N_PartTimeJob0 / (double) noEntrepreneur; // 0.29
-        // double P_X8_FemaleYesJob = (double) female_Y_PartTimeJob0 / (double) noEntrepreneur; // 0.02
-        // double P_X8_FemaleNoJob = (double) female_N_PartTimeJob0 / (double) noEntrepreneur; // 0.17
-
-        // double P_X9_MaleUrban = (double) maleUrban0 / (double) noEntrepreneur; // 0.40
-        // double P_X9_MaleRural = (double) maleRural0 / (double) noEntrepreneur; // 0.40
-        // double P_X9_FemaleUrban = (double) femaleUrban0 / (double) noEntrepreneur; // 0.04
-        // double P_X9_FemaleRural = (double) femaleRural0 / (double) noEntrepreneur; // 0.16
-
-        // double P_X10_MaleYesStudyBusiness = (double) male_Y_StudyBusiness0 / (double) noEntrepreneur; // 0.40
-        // double P_X10_MaleNoStudyBusiness = (double) male_N_StudyBusiness0 / (double) noEntrepreneur; // 0.40
-        // double P_X10_FemaleYesStudyBusiness = (double) female_Y_StudyBusiness0 / (double) noEntrepreneur; // 0.05
-        // double P_X10_FemaleNoStudyBusiness = (double) female_N_StudyBusiness0 / (double) noEntrepreneur; // 0.15
-
-        // // SUBSTITUTE VALUES INTO NAIVE BAYES FORMULA
-        // double entrepreneurWithFeatures = 0;
-        // double denominator = PX_Male * PX_Y_OwnBusiness * PX_Y_PartTimeJob * PX_Urban * PX_N_StudyBusiness;
-
-        // if(gender == "Male")
-        // {
-        //     if(ownBusiness == "Yes")
-        //     {
-        //         if(partTimeJob == "Yes")
-        //         {
-        //             if(area == "Urban" || area == "Urban  ")
-        //             {
-        //                 if(studyBusiness == "Yes")
-        //                 {
-        //                     entrepreneurWithFeatures = (P_X1_Male * P_X2_MaleYesOwnBusiness * P_X3_MaleYesJob * P_X4_MaleUrban * P_X5_MaleYesStudyBusiness) / denominator; 
-        //                 }
-        //                 else if(studyBusiness == "No")
-        //                 {
-        //                     entrepreneurWithFeatures = (P_X1_Male * P_X2_MaleYesOwnBusiness * P_X3_MaleYesJob * P_X4_MaleUrban * P_X5_MaleYesStudyBusiness) / denominator;  
-        //                 }
-        //             }
-        //             else if(area == "Rural")
-        //             {
-
-        //             }
-        //         }
-        //     }
+        // GETTING NUMERATOR FOR ENTREPRENEUR AND NO ENTREPRENEUR + DENOMINATOR 
+        // (1 IN THE VARIABLE NAME MEANS ENTREPRENEUR)
+        // (0 IN THE VARIABLE NAME MEANS NO ENTREPRENEUR)
+        if("Male".equals(gender))
+        {
+            numerator1 = (double) probabilitiesYesEntrepreneur.get("maleProbability1");
+            numerator0 = (double) probabilitiesNoEntrepreneur.get("maleProbability0");
+            denominator = PX_Male;
+        }
+        else if("Female".equals(gender))
+        {
+            numerator1 = (double) probabilitiesYesEntrepreneur.get("femaleProbability1");
+            numerator0 = (double) probabilitiesNoEntrepreneur.get("femaleProbability0");
+            denominator = PX_Female;
+        }
             
-        // }
+        if("Yes".equals(ownBusiness))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("yesOwnBusinessProbability1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("yesOwnBusinessProbability0");
+            denominator *= PX_Y_OwnBusiness;
+        }
+        else if("No".equals(ownBusiness))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("noOwnBusinessProbability1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("noOwnBusinessProbability0");
+            denominator *= PX_N_OwnBusiness; 
+        }
+
+        if("Yes".equals(partTimeJob))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("yesPartTimeJob1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("yesPartTimeJob0");
+            denominator *= PX_Y_PartTimeJob;
+        }
+        else if("No".equals(partTimeJob))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("noPartTimeJob1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("noPartTimeJob0");
+            denominator *= PX_N_PartTimeJob; 
+        }
+
+        if("Rural".equals(area))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("rural1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("rural0");
+            denominator *= PX_Rural;
+        }
+        else if("Urban".equals(area))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("urban1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("urban0");
+            denominator *= PX_Urban; 
+        }
+
+        if("Yes".equals(studyBusiness))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("yesStudyBusiness1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("yesStudyBusiness0");
+            denominator *= PX_Y_StudyBusiness;
+        }
+        else if("No".equals(studyBusiness))
+        {
+            numerator1 *= (double) probabilitiesYesEntrepreneur.get("noStudyBusiness1");
+            numerator0 *= (double) probabilitiesNoEntrepreneur.get("noStudyBusiness0");
+            denominator *= PX_N_StudyBusiness; 
+        }
+
+        numerator1 *= P_Y_Entrepreneur;
+        numerator0 *= P_N_Entrepreneur;
+
+        // SUBSTITUTE VALUES TO NAIVE BAYES FORMULA
+        probabilityYesEntrepreneur = numerator1 / denominator;
+        probabilityNoEntrepreneur = numerator0 / denominator;
+
+        // PRINT TESTS 
+        System.out.print("Yes ENT: ");
+        System.out.printf("%.2f ", probabilityYesEntrepreneur);
+        System.out.print("No ENT: ");
+        System.out.printf("%.2f \n", probabilityNoEntrepreneur); 
         
+        probabilityYesEntrepreneur *= 100; 
+        probabilityNoEntrepreneur *= 100;
 
-        // // PRINT TESTS 
-        // System.out.println(male1 + " " + female1 + " " + yesOwnBusiness1 + " " + noOwnBusiness1 + " " + yesPartTimeJob1 + " " + 
-        //                    noPartTimeJob1 + " " + urban1 + " " + rural1 + " " + yesStudyBusiness1 + " " + noStudyBusiness1);
+        // ANSWERS
+        if(probabilityYesEntrepreneur > probabilityNoEntrepreneur)
+        {
+            answer = "Entrepreneur, their probabilities of becoming an entrepreneur are: " +  probabilityYesEntrepreneur;
+        }
+        else
+        {
+            answer = "No Entrepreneur, their probabilities of NOT becoming an entrepreneur are: " + probabilityNoEntrepreneur;
+        }
 
-        System.out.println(male0 + " " + female0 + " " + yesOwnBusiness0 + " " + noOwnBusiness0 + " " + yesPartTimeJob0 + " " + 
-        noPartTimeJob0 + " " + urban0 + " " + rural0 + " " + yesStudyBusiness0 + " " + noStudyBusiness0);
-        // System.out.println(male_Y_Entrepreneur1);
-        // System.out.printf("%.10f \n", entrepreneurWithFeatures);
-        // // System.out.printf("%.2f \n", P_X10_FemaleNoStudyBusiness);
-        // // System.out.println("Current data YES: " + currentDataWithSameFeaturesAndYesEntrepreneur + "\nCurrent data NO: " + currentDataWithSameFeaturesAndNoEntrepreneur);
-        // System.out.println("BIG TEST OF DATA: \n" + "No ENTRE:" + noEntrepreneur +
-        // " NOENT:" + male_N_Entrepreneur0 +" YESBUS:"+
-        // male_Y_OwnBusiness0 +" NOBUS:"+ male_N_OwnBusiness0 +" YESJOB:"+
-        // male_Y_PartTimeJob0 +" NOJOB:"+ male_N_PartTimeJob0 +" URB:"+
-        // maleUrban0 +" RUR:"+ maleRural0 + " YESBUSI:"+
-        // male_Y_StudyBusiness0 +" NOBUSI:"+ male_N_StudyBusiness0
-        // );
-
-        // // System.out.println("MALE YES ENT: " + male_Y_Entrepreneur + " " + "FEMALE YES ENT: " + female_Y_Entrepreneur);
-        // // System.out.println("MALE YES COMP : " + male_Y_OwnBusiness + " " + "FEMALE YES COMP : " + female_Y_OwnBusiness);
-
-        // // System.out.printf("%.2f", PX_N_StudyBusiness);
-        
     } 
+
+    public String toString() 
+    {
+        return (answer);
+    }
         
 }
